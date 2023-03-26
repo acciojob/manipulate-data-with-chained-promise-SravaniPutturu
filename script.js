42ba3236a6e873913ce3b22515bdd13e0c58ef5e
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-26 15:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+var promise = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve([1, 2, 3, 4])
+	},3000)
+})
+promise
+.then((result)=>{
+	return result.filter(ele)=>ele % 2 == 0;	
+})
+.then((result)=>{
+	 var mul = result.filter(ele)=>ele % 2 == 0;
+	retuen mul * 2;
+})
+
+.then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = mul;
+  })
+
+//document.getElementById('output').innerText = mul;
+
+
+
+
