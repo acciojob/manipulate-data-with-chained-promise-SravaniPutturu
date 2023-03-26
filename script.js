66843548e8ba0a78240c7ea06e4c58d74aeb24ac
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-26 16:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// document.getElementById('output').innerText = "jnkln";
+// var promise = new Promise((resolve,reject)=>{
+// 	setTimeout(()=>{
+// 		resolve([1, 2, 3, 4])
+// 	},3000)
+// })
+// promise
+// .then((result)=>{
+// 	return result.filter(ele)=>ele % 2 == 0;	
+// })
+// .then((result)=>{
+// 	 var mul = result.filter(ele)=>ele % 2 == 0;
+// 	retuen mul * 2;
+// })
 
-var promise = new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve([1, 2, 3, 4])
-	},3000)
-})
+// .then((result) => {
+//     const outputDiv = document.getElementById('output');
+//     outputDiv.textContent = mul;
+//   })
+
+
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(numbersArray);
+  }, 3000);
+});
+
 promise
-.then((result)=>{
-	return result.filter(ele)=>ele % 2 == 0;	
-})
-.then((result)=>{
-	 var mul = result.filter(ele)=>ele % 2 == 0;
-	retuen mul * 2;
-})
-
-.then((result) => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = mul;
+  .then(numbersArray => {
+    const filteredArray = numbersArray.filter(num => num % 2 === 0);
+    return filteredArray;
   })
+  .then(filteredArray => {
+    const multipliedArray = filteredArray.map(num => num * 2);
+    return multipliedArray;
+  })
+  .then(finalArray => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = finalArray.join(', ');
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
-//document.getElementById('output').innerText = mul;
+
+
 
 
 
